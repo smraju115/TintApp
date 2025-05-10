5445e20bcb7d4454483bb3530776e02676025be8
Raju <smraju115@gmail.com>
2025-05-10 18:23:52 +0600

    UpdateDesign

--- TintApp/obj/Debug/net8.0/TintApp.AssemblyInfo.cs
+++ TintApp/obj/Debug/net8.0/TintApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TintApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1a383394ce47b91f29a9188705d1eb811c14b87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82a54858cd39c394074e43476f3f6a20d09b3f72")]
 [assembly: System.Reflection.AssemblyProductAttribute("TintApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TintApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
